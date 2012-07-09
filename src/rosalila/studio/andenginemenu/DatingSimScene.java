94b0110ecf6030d798e3ac099383fae9f6e94172
Jerzon Hernandez <joel.hz24@gmail.com>
2012-07-08 19:01:16 -0600

    mas assets

--- src/rosalila/studio/andenginemenu/DatingSimScene.java
+++ src/rosalila/studio/andenginemenu/DatingSimScene.java
@@ -7,7 +7,6 @@ import org.andengine.audio.music.MusicFactory;
 import org.andengine.audio.music.MusicManager;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
-//import org.andengine.examples.MusicExample;
 import org.andengine.opengl.texture.TextureManager;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
@@ -20,6 +19,7 @@ import android.content.Context;
 
 public class DatingSimScene extends Scene {
 	private Sprite bg;
+	private Sprite girl;
 	private Music music;
 
 	public DatingSimScene()
@@ -27,10 +27,15 @@ public class DatingSimScene extends Scene {
 		super();
 		this.setBackgroundEnabled(false);
 		BitmapTextureAtlas mBitmapTextureAtlas = new BitmapTextureAtlas((TextureManager) Global.getResource(Global.TEXTURE_MANAGER), 720, 480, TextureOptions.BILINEAR);
-		ITextureRegion mDatingSimTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, (Context)Global.getResource(Global.MAIN_ACTIVITY), "datingsim_background.png", 0, 0);
+		BitmapTextureAtlas mBitmapTextureAtlas2 = new BitmapTextureAtlas((TextureManager) Global.getResource(Global.TEXTURE_MANAGER), 720, 480, TextureOptions.BILINEAR);
+		ITextureRegion mDatingSimTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, (Context)Global.getResource(Global.MAIN_ACTIVITY), "dating_sim/bg3.png", 0, 0);
+		ITextureRegion mDatingSimTextureRegion2 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas2, (Context)Global.getResource(Global.MAIN_ACTIVITY), "dating_sim/chicas/enfermera_normal.png", 0, 0);
 		mBitmapTextureAtlas.load();
+		mBitmapTextureAtlas2.load();
 		bg= new Sprite(0, 0, mDatingSimTextureRegion, (VertexBufferObjectManager)Global.getResource(Global.VERTEX_BUFFERED_OBJECT_MANAGER));
+		girl= new Sprite(0, 0, mDatingSimTextureRegion2, (VertexBufferObjectManager)Global.getResource(Global.VERTEX_BUFFERED_OBJECT_MANAGER));
 		this.attachChild(bg);
+		this.attachChild(girl);
 		
 		try {
 			this.music = MusicFactory.createMusicFromAsset((MusicManager)Global.getResource(Global.MUSIC_MANAGER), (Context)Global.getResource(Global.MAIN_ACTIVITY), "chuy_invasion.ogg");
